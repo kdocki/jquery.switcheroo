a2afb29a6fe5a83115512200562bc6c48e14fa2c
Kelt Dockins <kelt@dockins.org>
2014-03-10 09:20:49 -0500

    adding gruntfile and stylesheets

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function(grunt) {
+
+  // load all grunt tasks
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+  grunt.initConfig({
+    bowerInstall: {
+      target: {
+      	src: [ 'examples/*.html', 'index.html' ]
+      }
+    },
+    watch: {
+      livereload: {
+        files: [
+          'examples/**',
+          "jquery.switcheroo.js"
+        ],
+        options: {
+          livereload: true
+        }
+      }
+    }
+
+  });
+
+  grunt.registerTask('default', 'watch');
+  grunt.registerTask('bower', 'bowerInstall');
+}
